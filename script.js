83d563251fa4b378e00f6a6b13f3e84298efd636
AyaAya-coder <aidynbulatov@gmail.com>
2024-10-15 12:39:00 +0500

    2

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 const btn = document.getElementById('button')
 const input = document.getElementById('input')
 const body = document.getElementById('body')
+const mainContent = document.getElementById('main_content')
+const thereIsNothing = document.getElementById('there_is_nothing')
 btn.addEventListener('click', ()=>{
     if(input.value == "19915152"){
         body.dataset.open = "true"
+        addElement1()
+    } else if(input.value == "Alexandr"){
+        body.dataset.open = "true"
+        addElement2()
     } else {
         alert("not right")
     }
 })
+
+function addElement1() {
+    const content = document.createElement("div");
+
+    const newContent = document.createTextNode("12|4 9|4 26|4 26|4 29|4");
+
+    content.appendChild(newContent);
+
+    mainContent.insertBefore(content, thereIsNothing);
+}
+
+function addElement2() {
+    const content = document.createElement("div");
+
+    const newContent = document.createTextNode("not bad");
+
+    content.appendChild(newContent);
+
+    mainContent.insertBefore(content, thereIsNothing);
+}
